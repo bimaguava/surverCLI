32a6f2bab99b34b85475c327adbb3954dc29cc9b
Seperkopian Jakarta <awetsemoga38@gmail.com>
2020-12-15 15:14:02 +0700

    Lagi iseng

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#newspaper3k
+pip3 install newspaper3k
+curl https://raw.githubusercontent.com/codelucas/newspaper/master/download_corpora.py | python3
+
+#readability-cli
+npm install -g readability-cli
+
+
